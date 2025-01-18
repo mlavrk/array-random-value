46aeba5b6afbe4a8b781a247f6e23b37c3e56524
Mike L <git@lavrik.in.ua>
2025-01-18 07:28:16 +0200

    Enhance docblock

--- src/array_random_value.php
+++ src/array_random_value.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * @param array $array
- * @return mixed
+ * Returns a random value from the given array.
+ *
+ * @param array $array Input array.
+ * @return mixed|null A random value from the array or null if the array is empty.
  */
 function array_random_value(array $array)
 {
